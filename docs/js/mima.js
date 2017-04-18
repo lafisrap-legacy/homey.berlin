7e9f46643fdfc8c77a3e7b90acb18e770cc50b28
Michael <michael@c2064.org>
2017-04-18 09:37:24 +0200

    Adding photos to shopping cart

--- docs/js/mima.js
+++ docs/js/mima.js
@@ -1,12 +1,42 @@
 // MAMI Javascript extensions
 
 $( () => {
-	$( "#startingpoints .nav-tabs li" ).click((e) => {
-		var href = e.target.getAttribute("href"),
-				dest = $( href + " ul li:first-child span" );
 
-		setTimeout( () => {
-			dest.trigger("click")
-		}, 200 );
+	// Image Gallery - Tabs
+	$( "#startingpoints .nav-tabs li" ).click((e) => {
+		var href = e.target.getAttribute("href");
+
+		$( href + " ul li:first-child span" ).trigger("click");
+	});
+
+	// Image Gallery - Checkboxes
+	$( ".checkbox-modal input" ).change((e) => {
+		let id = e.target.getAttribute("checkbox");
+		$(".checkbox-modal input[checkbox='"+id+"']").prop('checked', e.target.checked);
+		$("#portfoliolist .mix[checkbox='"+id+"']" ).toggleClass("selected");
+
+		let checked = [];
+
+		$( "#portfoliolist .checkbox-modal input" ).filter(function(elem) { 
+			if( $(this).is(":checked") ) 
+				checked.push({
+					image: $(this).attr("img"),
+					id: $(this).attr("img-id")
+				});
+		});
+
+		if( checked.length ) {
+			$( ".navbar-name" ).text( checked.length + " image/s selected." );
+		}
+
+		let images = "";
+
+		$.each(checked, (index, image) => {
+			images += '<div class="image"><img src="'+image.image+'" class="img-responsive" /></div>';
+		})		
+
+		$( "#contact .thumbnails").html(images);
+
+		document.mamiImagesChecked = checked;
 	});
 });
